ab70b5a0f1fc36b8762b33267c995c46f6e76769
subhamkumarsahoo1600 <subhamkumarsahoo1600@gmail.com>
2026-02-02 14:56:52 +0530

    dfd

--- utils/sendAnyMailToMe.js
+++ utils/sendAnyMailToMe.js
@@ -6,7 +6,7 @@ const sendAnyMailToMe = async ({ mail, subject, html }) => {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
-            secure: false, // true for 465, false for other ports
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
